18a14f9a992503b947b0d56e32e6495faae7c97f
ichigo-dev <ichigo@web-freework.com>
2021-07-26 15:03:41 +0900

    update: ゲーム開始時のモーダルを作成

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 const Home = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <div className="us-center">
-        <Link to="/game" className="us-tdn">
-            <Button variant="success" className="us-l-btn">
-              ゲームを始める
-            </Button>
-        </Link>
+        <Button variant="success" className="us-l-btn" onClick={ () => handleShow() }>
+          ゲームを始める
+        </Button>
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>ゲームを始める</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Woohoo, you're reading this text in a modal!
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            閉じる
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            スタート
+          </Button>
+        </Modal.Footer>
+      </Modal>
       </div>
     </>
   );
