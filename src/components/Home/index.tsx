2c921ed5d8a763492ac29496ecc3cd6878cfe5f2
ichigo-dev <ichigo@web-freework.com>
2021-08-08 22:38:40 +0900

    Update: ニックネーム登録フォームを追加

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Tabs, Tab, Form } from 'react-bootstrap';
 
 const Home = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const startBtnDisabled: boolean = true;
 
   return (
     <>
@@ -14,21 +16,47 @@ const Home = () => {
           ゲームを始める
         </Button>
         <Modal show={show} onHide={handleClose}>
-        <Modal.Header>
-          <Modal.Title>ゲームを始める</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Woohoo, you're reading this text in a modal!
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            閉じる
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            スタート
-          </Button>
-        </Modal.Footer>
-      </Modal>
+          <Modal.Header>
+            <Modal.Title>ゲームを始める</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tabs defaultActiveKey="single" id="uncontrolled-tab-example" className="mb-3">
+              <Tab eventKey="single" title="1人で遊ぶ">
+                <Form.Group className="mb-3" controlId="formName">
+                  <Form.Label>ニックネーム</Form.Label>
+                  <Form.Control type="text" placeholder="例）太郎" />
+                  <Form.Text className="alert-text">
+                    すでに存在するニックネームです。あなたは太郎ですか？
+                  </Form.Text>
+                </Form.Group>
+              </Tab>
+              <Tab eventKey="doubles" title="2人で遊ぶ">
+                <Form.Group className="mb-3" controlId="formName1">
+                  <Form.Label>ニックネーム（1人目）</Form.Label>
+                  <Form.Control type="text" placeholder="例）太郎" />
+                  <Form.Text className="alert-text">
+                    すでに存在する名前です。あなたは太郎ですか？
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formName2">
+                  <Form.Label>ニックネーム（2人目）</Form.Label>
+                  <Form.Control type="text" placeholder="例）太郎" />
+                  <Form.Text className="alert-text">
+                    すでに存在する名前です。あなたは太郎ですか？
+                  </Form.Text>
+                </Form.Group>
+              </Tab>
+            </Tabs>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              閉じる
+            </Button>
+            <Button variant="primary" onClick={handleClose} disabled={startBtnDisabled}>
+              スタート
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </>
   );
